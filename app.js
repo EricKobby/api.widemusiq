d5abc6caeb6a2c5c4da8d9c45de1b99a4d0adf25
Eric Kobby <mensaheric743@hotmail.com>
2019-12-03 23:58:59 +0000

    upload tested and working

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+//setting up express-fileupload middleware
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//setting up cors middleware
+app.use(cors());
+
+//setting up body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//importing routes
+const tracksRouter = require('./routes/tracks');
+
+const port = process.env.PORT || 1455;
+app.use('/api/tracks',tracksRouter);
+app.listen(port,()=>{
+    console.log("Api Running on port ".concat(port));
+});
+
---
+++ routes/tracks.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+const sqlClient = require('../SqlClient');
+
+router.get('/', (req, res) => {
+    sqlClient.query('SELECT * FROM tracks', (err, rows) => {
+        if (err) res.status(900)
+            .json(ErrorMessage(err));
+        res.status(200).json({
+            error: false,
+            tracks: rows
+        });
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    sqlClient.query('SELECT * FROM tracks WHERE Id = ?', [id], (err, result) => {
+        if (err) res.status(900).json(ErrorMessage(err));
+        res.status(200).json({
+            error: false,
+            track: result
+        });
+    })
+});
+
+router.post('/add', async (req, res) => {
+    if (!req.files) {
+        res.send({
+            status: false,
+            message: 'No file uploaded'
+        });
+    }
+    else {
+
+        let { songfile, thumbnail } = req.files;
+        let { songname,artistname } = req.body;
+        songfile.mv('../widemusiq/public/songs/' + songfile.name);
+        thumbnail.mv('../widemusiq/public/thumbnails/' + thumbnail.name);
+        //saving details to database
+        const data =[
+            songname,
+            thumbnail.name,
+            songfile.name,
+            artistname
+        ];
+        sqlClient.query('INSERT INTO tracks (`song_name`,`thumbnail`,`file_name`,`artist_name`) VALUES (?,?,?,?)',data,(err,result)=>{
+            if(err) res.status(301).json(ErrorMessage(err));
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                request: req.body,
+                data: [
+                    {
+                        name: songfile.name,
+                        mimetype: songfile.mimetype,
+                        size: songfile.size
+                    },
+                    {
+                        name: thumbnail.name,
+                        mimetype: thumbnail.mimetype,
+                        size: thumbnail.size
+                    }
+                ]
+            });
+        });
+    }
+});
+
+const ErrorMessage = err => ({
+    error: true,
+    message: err.message
+});
+module.exports = router;
